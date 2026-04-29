db899da91274f327c03d902ebac47752f1ad156f
wishyrater <julian.cesar.andersen@gmail.com>
2026-04-29 19:41:56 +0200

    Simply ship and fix tests

--- src/Ship.js
+++ src/Ship.js
@@ -1,7 +1,5 @@
 export default class Ship {
-    constructor(x, y, size, hits = 0, sunken = false) {
-        this.x = x;
-        this.y = y;
+    constructor(size, hits = 0, sunken = false) {
         this.size = size;
         this.hits = hits;
         this.sunken = sunken;
--- src/__tests__/Ship.test.js
+++ src/__tests__/Ship.test.js
@@ -1,28 +1,28 @@
 import Ship from "../Ship";
 
 test("isSunk returns true", () => {
-    expect(new Ship(null, null, 1, 1).isSunk()).toBe(true);
+    expect(new Ship(1, 1).isSunk()).toBe(true);
 });
 
 test("isSunk returns false", () => {
-    expect(new Ship(null, null, 2, 1).isSunk()).toBe(false);
+    expect(new Ship(2, 1).isSunk()).toBe(false);
 });
 
 test("hit registers", () => {
-    const ship = new Ship(null, null, 3);
+    const ship = new Ship(3);
     ship.hit();
     expect(ship.hits).toBe(1);
 });
 
 test("multiple hits register", () => {
-    const ship = new Ship(null, null, 3);
+    const ship = new Ship(3);
     ship.hit();
     ship.hit();
     expect(ship.hits).toBe(2);
 })
 
 test("ship sinks after enough hits", () => {
-    const ship = new Ship(null, null, 2);
+    const ship = new Ship(2);
     ship.hit();
     ship.hit();
     expect(ship.isSunk()).toBe(true);
