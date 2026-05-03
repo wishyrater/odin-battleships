0379690e32742b961fa63f890ecc3cab7136cb07
wishyrater <julian.cesar.andersen@gmail.com>
2026-05-03 14:23:42 +0200

    Add dependencies for html and css to webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -11,12 +11,33 @@ export default {
     },
     plugins: [
         new HtmlWebpackPlugin({
-        title: 'Battleships',
+            template: "./src/template.html",
+            title: 'Battleships',
         }),
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpeg|jpg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
 };
